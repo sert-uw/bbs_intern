4a3af6970e7a6bf465876db9627fc7276211537f
sert <demo.sert@gmail.com>
2015-08-22 20:11:30 +0900

    create test

--- spec/models/bbs_thread_spec.rb
+++ spec/models/bbs_thread_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe BbsThread, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'correct parameters' do
+      it { expect(build(:bbs_thread)).to be_valid }
+    end
+
+    context 'wrong parameters' do
+      it { expect(build(:bbs_thread, name: nil)).not_to be_valid }
+      it { expect(build(:bbs_thread, address: nil)).not_to be_valid }
+      it { expect(build(:bbs_thread, title: nil)).not_to be_valid }
+      it { expect(build(:bbs_thread, body: nil)).not_to be_valid }
+      it { expect(build(:bbs_thread, password: nil)).not_to be_valid }
+    end
+  end
 end
