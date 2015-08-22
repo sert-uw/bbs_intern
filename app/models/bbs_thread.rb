0bc05aecdd585dc64273b387ea6228fa6d869cfa
sert <demo.sert@gmail.com>
2015-08-22 18:51:21 +0900

    add validates

--- app/models/bbs_thread.rb
+++ app/models/bbs_thread.rb
@@ -1,3 +1,9 @@
 class BbsThread < ActiveRecord::Base
   has_many :responses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :password, presence: true
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,8 @@
 class Response < ActiveRecord::Base
   belongs_to :bbs_thread
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :body, presence: true
+  validates :password, presence: true
 end
