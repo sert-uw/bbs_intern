9892fa29a3466e73db15f07e22f7742445adac52
sert <demo.sert@gmail.com>
2015-08-22 22:33:23 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'bbs_threads#index'
 
-  resources :bbs_threads do
-    resources :responses
+  resources :bbs_threads, only: [:index, :new, :show, :create, :destroy] do
+    resources :responses, only: [:create, :destroy]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
