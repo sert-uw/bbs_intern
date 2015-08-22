9a1328fa845535dd7ff79ecae66eee273c4b3347
sert <demo.sert@gmail.com>
2015-08-22 22:20:13 +0900

    fix controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,51 +1,18 @@
 class ResponsesController < ApplicationController
-  before_action :set_response, only: [:show, :edit, :update, :destroy]
-
-  # GET /responses
-  # GET /responses.json
-  def index
-    @responses = Response.all
-  end
-
-  # GET /responses/1
-  # GET /responses/1.json
-  def show
-  end
-
-  # GET /responses/new
-  def new
-    @response = Response.new
-  end
-
-  # GET /responses/1/edit
-  def edit
-  end
+  before_action :set_thread
+  before_action :set_response, only: [:destroy]
 
   # POST /responses
   # POST /responses.json
   def create
-    @response = Response.new(response_params)
+    @response = @thread.responses.build(response_params)
 
     respond_to do |format|
       if @response.save
-        format.html { redirect_to @response, notice: 'Response was successfully created.' }
+        format.html { redirect_to bbs_thread_path(@thread.id), notice: 'Response was successfully created.' }
         format.json { render :show, status: :created, location: @response }
       else
-        format.html { render :new }
-        format.json { render json: @response.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /responses/1
-  # PATCH/PUT /responses/1.json
-  def update
-    respond_to do |format|
-      if @response.update(response_params)
-        format.html { redirect_to @response, notice: 'Response was successfully updated.' }
-        format.json { render :show, status: :ok, location: @response }
-      else
-        format.html { render :edit }
+        format.html { redirect_to bbs_thread_path(@thread.id) }
         format.json { render json: @response.errors, status: :unprocessable_entity }
       end
     end
@@ -56,12 +23,16 @@ class ResponsesController < ApplicationController
   def destroy
     @response.destroy
     respond_to do |format|
-      format.html { redirect_to responses_url, notice: 'Response was successfully destroyed.' }
+      format.html { redirect_to bbs_thread_path(@thread.id), notice: 'Response was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_thread
+      @thread = BbsThread.find(params[:bbs_thread_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_response
       @response = Response.find(params[:id])
