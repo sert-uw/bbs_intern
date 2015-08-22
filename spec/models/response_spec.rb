fccb68b1e18e19f0f7c25062e27a59927bc2c479
sert <demo.sert@gmail.com>
2015-08-22 20:11:30 +0900

    create test

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Response, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'correct parameters' do
+      it { expect(build(:response)).to be_valid }
+    end
+
+    context 'wrong parameters' do
+      it { expect(build(:response, name: nil)).not_to be_valid }
+      it { expect(build(:response, address: nil)).not_to be_valid }
+      it { expect(build(:response, body: nil)).not_to be_valid }
+      it { expect(build(:response, password: nil)).not_to be_valid }
+    end
+  end
 end
