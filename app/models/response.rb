5b4007d38ea309d0ba9a38dafaae8deeba0371d2
sert <demo.sert@gmail.com>
2015-08-23 00:02:00 +0900

    fix validate

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,7 +2,6 @@ class Response < ActiveRecord::Base
   belongs_to :bbs_thread
 
   validates :name, presence: true
-  validates :address, presence: true
   validates :body, presence: true
   validates :password, presence: true
 end
