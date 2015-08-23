e55bdd1df7d457857f476c9f209b5411b8f38063
sert <demo.sert@gmail.com>
2015-08-23 16:07:33 +0900

    create seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+200.times do |no|
+  thread = BbsThread.create(name: "投稿者 #{no}", address: "test#{no}@test.test", title: "スレッド #{no}", body: "本文 #{no}\nテスト", password: "password#{no}")
+
+  200.times do |res_no|
+    thread.responses.create(name: "投稿者 #{res_no}", address: "test#{res_no}@test.test", body: "コメント #{res_no}\nテスト", password: "password#{res_no}")
+  end
+end
